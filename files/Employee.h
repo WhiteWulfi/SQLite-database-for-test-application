7ab4c579138afed4de90d4194164376f74470ae2
Logan <92254749+WhiteWulfi@users.noreply.github.com>
2025-11-21 17:08:16 +0300

    Add files via upload

---
+++ files/Employee.h
@@ -0,0 +1,138 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <time.h>
+#include <sqlite3.h>
+
+#pragma warning(disable:4996) 
+
+// Вспомогательная функция принимающая слово затем возвращающая это же слово, но в нижнем регистре. 
+std::string tolower(const std::string& word)
+{
+	std::string result{ "" };
+	for (const char& symbol : word) result += std::tolower(symbol);
+	return result;
+}
+// Вспомогательная функция принимающее слово и исключённый символ затем возвращающая вариант слова, но без исключённого символа.
+std::string clear_word(const std::string& word, const char& banned)
+{
+	std::string result{ "" };
+	for (const char& symbol : word) ((symbol == banned) ? result += "" : result += symbol);
+	return result;
+}
+
+// Простая структура данных олицетворяющая дату организованную по шаблону: yyyy.mm.dd (Значение по умолчанию 2000.01.01.)
+struct Date
+{
+	Date() : year{ 2000 }, month{ 1 }, day{ 1 } {}
+	Date(int y, int m, int d) : year{ y }, month{ m }, day{ d } {}
+
+	int year{}, month{}, day{};
+};
+// Вспомогательная функция проверяющая дату на логическую верность (Не учитывает високосный год)
+bool is_correct_date(const Date& date)
+{
+	// условия для того, чтобы дата была верной - год > 0 и 12 >= месяц > 0 и 31 >= день > 0 
+	if ((date.year < 0) || (date.month < 1 || date.month > 12) || (date.day < 1 || date.day > 31)) return false;
+	return true;
+}
+
+// Класс олицетворяющий сущность типа 'сотрудник' (То есть единицу данных в таблице по учёту сотрудников) 
+class Employee
+{
+public:
+	Employee() : full_name{ "" }, date_of_birth{ Date() }, gender{ false } {}
+	Employee(const std::string& fn, const Date& dob, const bool& g) : full_name{ fn }, date_of_birth{ dob }, gender{ g }
+	{
+		if (!is_correct_date(date_of_birth)) std::cerr << "Incorrect date in employee: " << full_name << "\n\n";
+	}
+
+	const std::string& get_full_name() const { return full_name;     }
+	const Date& get_date_of_birth()    const { return date_of_birth; }
+	const bool& get_gender()           const { return gender;        }
+
+	void set_full_name(const std::string& fn) { full_name = fn; }
+	void set_date_of_birth(const Date& dob)
+	{
+		if (!is_correct_date(dob)) std::cerr << "Incorrect date in employee: " << full_name << "\n\n";
+		date_of_birth = dob;
+	}
+	void set_gender(const bool& g)           { gender = g;      }
+
+	// Метод рассчитывающий возраст сотрудника
+	const int calculate_age() const
+	{
+		time_t theTime = time(NULL);
+		struct tm* aTime = localtime(&theTime);
+		return ((aTime->tm_year + 1900) - date_of_birth.year);
+	}
+
+	// Метод отправляющий данного сотрудника в конкретную таблицу находящуюся в заданной базе данных
+	void insert_into_table(sqlite3* db, const std::string& table_name) const
+	{
+		// Проверка на правильность входных данных, иначе сотрудник не добавляется
+		if ((full_name == "") || (!is_correct_date(date_of_birth))) return;
+
+		// Запрос на добавление сотрудника пошагово собирается на языке SQL
+		std::stringstream employee_values{""};
+		employee_values << full_name << "', '" << date_of_birth.year << '-' << date_of_birth.month << '-' << date_of_birth.day << "', '" << (gender ? "Male" : "Female");
+		std::string sql_command{ "INSERT INTO " + table_name + " (Full_name, Date_of_birth, Gender) VALUES ('" + employee_values.str() + "');" };
+
+		// Попытка выполнить составленный запрос
+		char* error_message{ nullptr };
+		if ((sqlite3_exec(db, sql_command.c_str(), 0, 0, &error_message)) != SQLITE_OK)
+		{
+			// Если отправка завершилась ошибкой, то выводится сообщение об ошибке
+			std::cerr << "SQL error: " << error_message << '\n';
+			sqlite3_free(error_message);
+		}
+		// Если отправка завершилась успешно, то сотрудник добавлен и выводится сообщение об успехе операции
+		else std::cout << "Employee inserted succesefully in table " << table_name << "\n";
+	}
+private:
+	//Сущность 'сотрудник' состоит из полей full_name (ФИО), date_of_birth (Дата рождения) и gender (Пол)
+	std::string full_name{ "" };
+	Date date_of_birth{};
+	bool gender{};
+};
+
+// Перегрузка оператора вывода << для класса Date по шаблону: yyyy-mm-dd
+std::ostream& operator << (std::ostream& out, const Date& date)
+{
+	return out << date.year << '-' << std::setw(2) << std::setfill('0') << date.month << '-' << std::setw(2) << std::setfill('0') << date.day << std::setfill(' ');
+}
+// Перегрузка оператора вывода << для класса Employee по шаблону: | full_name | date_of_birth | Male/Female |
+std::ostream& operator << (std::ostream& out, const Employee& employee)
+{
+	out << "| " << std::setw(35) << employee.get_full_name() << " | " << employee.get_date_of_birth() << " | " << std::setw(8);
+	return (employee.get_gender() ? out << "Male |" : out << "Female |");
+}
+
+// Перегрузка оператора ввода >> для классов Date по шаблону: yyyy-mm-dd, где '-' любой символ
+std::istream& operator >> (std::istream& in, Date& date)
+{
+	char separator{ ' ' };
+	int year{ 2000 }, month{ 1 }, day{ 1 };
+	in >> year >> separator >> month >> separator >> day;
+
+	date = Date(year, month, day);
+	return in;
+}
+// Перегрузка оператора ввода >> для класса Employee по шаблону: "full_name" date_of_birth Male/Female
+std::istream& operator >> (std::istream& in, Employee& employee)
+{
+	std::string surname{ "" }, name{ "" }, patronymic{ "" };
+	Date date{};
+	std::string gender{ "" };
+
+	in >> surname >> name >> patronymic >> date >> gender;
+
+	employee = Employee(clear_word(surname, '\"') + " " + name + " " + clear_word(patronymic, '\"'), date, ((tolower(gender) == "male") ? true : false));
+	return in;
+}
+
+#endif
---
+++ files/Employee_register.h
@@ -0,0 +1,209 @@
+#ifndef EMPLOYEE_REGISTER_H
+#define EMPLOYEE_REGISTER_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <sqlite3.h>
+
+#include "Employee.h"
+
+// Массивы для случайной генерации ФИО.
+const std::string surnames[26]
+{ "Aaronson", "Backer", "Chalkley", "Dalman", "Edwards", "Flynn", "Godwin", "Harward", "Innes", "Jantz",
+  "Kovachev", "Letchford", "Matterson", "Nathaniel", "Overstreet", "Perkins", "Quickenden", "Rakestraw", "Sharman", "Twyman",
+  "Upshaw", "Vannoy", "Waering", "Xylander", "Yarranton", "Zachry"};
+const std::string names[20]
+{ "Maria", "Ekaterina", "Eva", "Anna", "Elizaveta", "Yasmina", "Victoria", "Svetlana", "Alexandra", "Maryana",
+"Artyom", "Ilya", "Timofey", "Svyatoslav", "Sergey", "Grigory", "Roman", "German", "Boris", "Stepan" };
+const std::string patronymics[10]
+{ "Ilyinichna", "Romanovna", "Glebovna", "Nikolaevna", "Eliseevna",
+"Daniilovich", "Yanovich", "Vladimirovich", "Arsentievich", "Tikhonovich" };
+
+// Вспомогательная функция генерирующая случайное ФИО, входной параметр - пол для генерируемого результата (Где 0 - это женский, а 1 - мужской)
+//(Первая половина массивов с именами и отчествами отведены только на женские вариации фио, а вторая - на мужские фио).
+std::string random_full_name(const int gender)
+{
+	return std::string((surnames[std::rand() % 26]) + " " + (names[(gender * 10) + std::rand() % 10]) + " " + (patronymics[(gender * 5) + std::rand() % 5]));
+}
+// Вспомогательная функция генерирующая частично случайное ФИО ('Частично случайное' из-за условия тестового задания).
+std::string special_full_name()
+{
+	return std::string(surnames[5] + " " + names[10 + (std::rand() % 10)] + " " + patronymics[5 + (std::rand() % 5)]);
+}
+// Вспомогательная функция генерирующая случайную дату (С учётом логической верности).
+Date random_date() { return Date(1990 + (std::rand() % 16), (std::rand() % 12) + 1, (std::rand() % 31) + 1); }
+
+// Вспомогательная функция отвечающая за возвращаемый результат (То есть единицу сущности типа 'сотрудник') при вызове операции SELECT в SQL.
+int callback(void* notUsed, int colCount, char** columns, char** colNames)
+{
+	// Результут пошагово собирается из получаемой информации.
+	std::stringstream data{ "" };
+	Employee employee{ };
+
+	for (int i = 0; i < colCount; i++) data << std::string(columns[i] ? columns[i] : "NULL") + " ";
+	data >> employee;
+
+	// Вывод собранного результата и расчёт возраста для сотрудника.
+	std::cout << employee << " " << employee.calculate_age() << " |\n";
+
+	return 0;
+}
+
+// Класс олицетворяющий сущность типа 'управляющий', что содержит базу данных задающуюся в конструкторе и организует большинство взаимодействий с ней.
+class Employee_register
+{
+public:
+	// Конструкторы создают базу данных и/или открывают поток сообщения с ней.
+	Employee_register() { open_data_base("Employee_register.db"); }
+	Employee_register(const std::string& db_name) { open_data_base(db_name + ".db"); }
+
+	// Метод создающий таблицу с указанным именем в текущей базе данных.
+	void create_table(const std::string& table_name = "Employee")
+	{
+		std::string sql_command{ "CREATE TABLE " + table_name + " (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Full_name TEXT NOT NULL, Date_of_birth TEXT NOT NULL, Gender TEXT NOT NULL);" };
+		char* error_message{ nullptr };
+
+		if ((sqlite3_exec(db, sql_command.c_str(), 0, 0, &error_message)) != SQLITE_OK)
+		{
+			std::cerr << "SQL error: " << error_message << "\n\n";
+			sqlite3_free(error_message);
+		}
+		else std::cout << "Table " << table_name << " created succesefully\n\n";
+	}
+
+	// Метод вставки сотрудника считанного извне в выбранную таблицу в текущей базе данных.
+	void insert_employee(const std::string& table_name = "Employee")
+	{
+		Employee employee{};
+		char exit{};
+
+		while (std::cin >> exit && exit != '0')
+		{
+			std::cin.putback(exit);
+			std::cin >> employee;
+			employee.insert_into_table(db, table_name);
+		}
+		clear_duplicates(table_name);
+		std::cout << '\n';
+	}
+
+	// Метод вывода всех сотрудников в консоль из указанной таблицы в текущей базе данных.
+	void show_all_employee(const std::string& table_name = "Employee")
+	{
+		std::string sql_command{ "SELECT Full_name, Date_of_birth, Gender FROM " + table_name + " GROUP BY Full_name, Date_of_birth" };
+		char* error_message{ nullptr };
+
+		if ((sqlite3_exec(db, sql_command.c_str(), callback, 0, &error_message)) != SQLITE_OK)
+		{
+			std::cerr << "SQL error: " << error_message << "\n\n";
+			sqlite3_free(error_message);
+		}
+		else std::cout << "The result above this statement\n\n";
+	}
+
+	// Метод добавляющий случайных 1000100 сотрудников в указанную таблицу в текущей базе данных.
+	void random_fill(const std::string& table_name = "Employee")
+	{
+		std::srand(std::time({}));
+
+		const unsigned size{ 1000100 };
+		Employee* employees{ new Employee[size] };
+
+		for (int size1 = 0; size1 < 100; size1++)
+		{
+		    int size2{ 0 + (10000 * size1) };
+			employees[size2] = Employee(special_full_name(), random_date(), true);
+		    
+			for (size2++; size2 < 10000 + (10000 * size1) + (1 * size1) + 1; size2++) 
+			{
+				const int gender{ std::rand() % 2 };
+				employees[size2] = Employee(random_full_name(gender), random_date(), (gender ? true : false));
+			}
+		}
+
+		batch_insertion(table_name, employees, size);
+
+		delete[] employees;
+	}
+
+	// Метод выводящий всех сотрудников в консоль подходящих по заданным условиям (ФИО начинается на F и пол мужской) из указанной таблицы в текущей базе данных.
+	void specific_select(const std::string& table_name = "Employee")
+	{
+		std::string sql_command{ "SELECT Full_name, Date_of_birth, Gender FROM " + table_name + " WHERE Full_name LIKE 'F%' AND Gender == 'Male';"};
+		char* error_message{ nullptr };
+
+		time_t start{}, end{};
+		time(&start);
+
+		if ((sqlite3_exec(db, sql_command.c_str(), callback, 0, &error_message)) != SQLITE_OK)
+		{
+			std::cerr << "SQL error: " << error_message << "\n\n";
+			sqlite3_free(error_message);
+		}
+		else
+		{
+			time(&end);
+			std::cout << "The result of custom select is above this statement and the time is - " << double(difftime(end, start)) << " seconds\n\n";
+		}
+	}
+
+	// Метод пакетной вставки в указанную таблицу массива сотрудников в текущей базе данных.
+	void batch_insertion(const std::string& table_name, const Employee* employees, const unsigned& size)
+	{
+		std::stringstream sql_command{ "" };
+		char* error_message{ nullptr };
+
+		sql_command << "BEGIN TRANSACTION; ";
+		for (unsigned counter = 0; counter < size; counter++)
+		{
+			sql_command << "INSERT INTO " + table_name + " (Full_name, Date_of_birth, Gender) VALUES ('"
+				        << employees[counter].get_full_name() << "', '"
+				        << employees[counter].get_date_of_birth().year << '-' << employees[counter].get_date_of_birth().month << '-' << employees[counter].get_date_of_birth().day << "', '"
+				        << (employees[counter].get_gender() ? "Male" : "Female")
+				        << "'); ";
+		}
+		sql_command << "COMMIT;";
+
+		if ((sqlite3_exec(db, sql_command.str().c_str(), 0, 0, &error_message)) != SQLITE_OK)
+		{
+			std::cerr << "SQL error: " << error_message << "\n\n";
+			sqlite3_free(error_message);
+		}
+		else
+		{
+			std::cout << "Batch insertion in table " << table_name << " completed succesefully\n";
+			clear_duplicates(table_name);
+		}
+	}
+
+	// Деструктор просто закрывает поток сообщения с текущей базой данных.
+	~Employee_register() { sqlite3_close(db); }
+private:
+	sqlite3* db{};
+
+	// Метод открывающий поток сообщения с указанной базой данных.
+	void open_data_base(const std::string& db_name)
+	{
+		// В случае если поток с базой данных уже открыт, то он будет закрыт и открыт новый.
+		if (db != nullptr) sqlite3_close(db);
+		if ((sqlite3_open(db_name.c_str(), &db)) != SQLITE_OK) std::cerr << "Error open DB: " << sqlite3_errmsg(db) << "\n\n";
+		else std::cout << "Database " << db_name << " opened succesefully\n\n";
+	}
+
+	// Метод очищающий указанную таблицу в текущей базе данных от повторяющихся сотрудников.
+	void clear_duplicates(const std::string& table_name = "Employee")
+	{
+		std::string sql_command{ "DELETE FROM " + table_name + " WHERE rowid NOT IN (SELECT MIN(rowid) FROM " + table_name + " GROUP BY Full_name);" };
+		char* error_message{ nullptr };
+
+		if ((sqlite3_exec(db, sql_command.c_str(), 0, 0, &error_message)) != SQLITE_OK)
+		{
+			std::cerr << "SQL error: " << error_message << "\n\n";
+			sqlite3_free(error_message);
+		}
+		else std::cout << "Duplicates in table " << table_name << " deleted succesefully\n\n";
+	}
+};
+
+#endif
